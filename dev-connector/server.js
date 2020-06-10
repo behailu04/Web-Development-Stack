378516aa36ea2652caf599675d21b1aba2279b55
Behailu Dereje <behailu04@gmail.com>
2020-06-10 18:45:05 +0300

    user and profile api

--- dev-connector/server.js
+++ dev-connector/server.js
@@ -1,7 +1,21 @@
-const express  = require('express')
+const express  = require('express');
+const connectDB = require('./config/db')
 
 const app = express()
+// connect Database
+connectDB();
 
-const PORT = process.env.PORT || 5000;
+// Init Middlware
+app.use(express.json({extended: false}));
+
+app.get('/', (req, res) => res.send('API Running'))
+
+// Define Routs
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
+app.use('/api/auth', require('./routes/api/auth'))
+
+const PORT = process.env.PORT || 5100;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
